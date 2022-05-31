f2f9cab46cbb26f541a90680b1a7d48013a9ab69
Tushar Singh <tusharvickey1999@gmail.com>
2022-05-31 19:59:06 +0530

    add error message styling fns
    Adds functions apply consistent styling for classnames and modules in error messages

--- src/utils.ts
+++ src/utils.ts
@@ -44,14 +44,24 @@ export const splitModuleSource = (source: string): { moduleSource: string; modul
 
 export class CSSModuleError extends Error {
     errorMessage: string
-    static path: NodePath
+    static path: NodePath | undefined
 
     constructor(errorMessage: string) {
         super()
         this.errorMessage = errorMessage
         this.name = chalk.red("CSSModuleError")
-        this.message = `at (${CSSModuleError.path.node.loc?.start.line}:${CSSModuleError.path.node.loc?.start.column})
+        this.message = `at (${CSSModuleError.path?.node.loc?.start.line}:${
+            CSSModuleError.path?.node.loc?.start.column
+        }): 
         ${this.errorMessage.replace(/ +/g, " ")}
         `.replace(/ +/g, " ")
+    }
+
+    static cls(cls: string) {
+        return `'${chalk.cyan(cls)}'`
+    }
+
+    static mod(mod: string) {
+        return `'${chalk.cyan(mod)}'`
     }
 }
